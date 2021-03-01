2758f8ca312c6137ef31b196e89c56d094ff95eb
Ming-Ho Yee <mh@mhyee.com>
2021-03-01 19:53:57 +0000

    Improve vtrace-shell

--- shell.sh
+++ shell.sh
@@ -6,6 +6,9 @@
 #
 # This script starts a local shell with the proper environment variables
 # defined; the variables will be unset when the shell exits.
+#
+# WARNING: This script will not work if it is symlinked; DIR will contain the
+# symlink's directory and not the original script's directory.
 ################################################################################
 
 # Don't start a vtrace-shell inside a vtrace-shell
@@ -24,9 +27,11 @@ export VTRACE_SHELL=1
 export R_ENABLE_JIT=0
 export R_DISABLE_BYTECODE=1
 
-# Custom prompt so we know we're in vtrace-shell
-export PS1="\[\033[01;35m\](vtrace-shell)\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]> "
-
-# Replace the current shell script process with bash,
-# inheriting all environment variables but without readining init scripts
-exec bash --noprofile --norc
+# Replace the current shell script process with bash, inheriting all
+# environment variables, sourcing .bashrc to retain aliases, and then setting a
+# custom prompt so we know we're in vtrace-shell.
+#
+# If we set the prompt before calling bash, then it will be overwritten by
+# sourcing ~/.bashrc. If we call bash with --norc --noprofile then we don't get
+# any aliases or functions defined in ~/.bashrc.
+exec bash --rcfile <(echo '. ~/.bashrc; PS1="\[\033[01;35m\](vtrace-shell)\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]> "')
